f037f26db08a2ad3e57221285d19ea07b18c3b16
awexli <awexli@mail.sfsu.edu>
2020-04-28 19:36:54 -0700

    complete email validation functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+const emailInput = document.getElementById('email-input-field');
+const emailError = document.getElementById('email-error');
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+document.querySelector('.btn-submit').addEventListener('click', (e) => {
+  if (!emailRegex.test(emailInput.value)) {
+    console.log('not email');
+    emailInput.style.border = '2px solid hsl(0, 93%, 68%)';
+    emailError.style.display = 'block';
+  } else {
+    emailInput.style.borderColor = '1px solid hsl(0, 36%, 70%)';
+    emailError.style.display = 'none';
+    emailInput.value = '';
+  }
+});
